c1f736f095aeee0357b3f2009227528841e93fd5
Amarumarcus <marchenko.k.e@gmail.com>
2026-03-26 14:22:13 +0100

    vitePrerender setup

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { createRequire } from 'module'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
 const require = createRequire(import.meta.url)
 const vitePrerender = require('vite-plugin-prerender')
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
   plugins: [
     react(),
     vitePrerender({
+      staticDir: path.resolve(__dirname, 'dist'),
       routes: ['/'],
     }),
   ],
