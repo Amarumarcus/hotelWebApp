1092479e1d4fe79a608641dd08c4a32e2298fd7d
Amarumarcus <marchenko.k.e@gmail.com>
2026-05-07 20:34:27 +0200

    change map

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,10 +1,10 @@
 import './Map.css'
 
-const MAP_URL =
-  'https://yandex.ru/map-widget/v1/?um=constructor%3A5c5b181aa775502b20833429ebd17d6d460d2bc9cc6bff51f0978103291d0c49&source=constructor'
+
+const MAP_IFRAME_URL = 
+  'https://yandex.ru/map-widget/v1/?ll=40.294067%2C43.180055&z=16&pt=40.294067%2C43.180055,pm2rdm~40.294067,43.180055,text:Reliz%20Hotel'
 
 export default function Map() {
-
   return (
     <section id="map" className="map-section">
       <div className="map-section__inner">
@@ -22,12 +22,12 @@ export default function Map() {
 
         <div className="map-section__map">
           <iframe
-            src={MAP_URL}
+            src={MAP_IFRAME_URL}
             title="Расположение гостиницы Релиз"
-            allowFullScreen
-            loading="lazy"
             width="100%"
             height="400"
+            allowFullScreen
+            loading="lazy"
           />
         </div>
 
